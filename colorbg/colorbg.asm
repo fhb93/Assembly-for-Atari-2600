a635be8c3c237ad38b11f24f728c46ee1f524b6a
Felipe Holanda Bezerra <felipe.h.bezerra@gmail.com>
2021-05-02 22:09:46 -0300

    Add files via upload

---
+++ colorbg/colorbg.asm
@@ -0,0 +1,25 @@
+    processor 6502
+
+    include "vcs.h"
+    include "macro.h"
+
+    seg code
+    org $F000          ; defines the origin of the ROM ad $F000
+
+START:
+    CLEAN_START        ; Macro to safely clear the memory
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Set background luminosity color to yellow
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #$1E           ; Load color into A ($1E is NTSC yellow)
+    sta COLUBK         ; store A to BackgroundColor Address $09 
+
+    jmp START          ; Repeat from START
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Fill ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $FFFC          ; Defines origin to $FFFC
+    .word START        ; Reset vector at $FFFC (where program starts)
+    .word START        ; Interrup vector at $FFFE (unused in the VCS)
